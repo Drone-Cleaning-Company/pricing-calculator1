b498931a7eaf8750c0ad3614ebf119c5511de614
Drone-Cleaning-Company <dronecleaningcompanyinterns@gmail.com>
2025-03-10 18:18:25 -0400

    8

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('./models/User');
+const User = require('../models/User');
 
 // Validate JWT_SECRET
 if (!process.env.JWT_SECRET) {
